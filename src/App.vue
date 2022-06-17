6389c36785433af79f8db1a4af0118ecb8a15724
Ikinidae <federicaelia24@gmail.com>
2022-06-17 12:28:31 +0200

    Creati componenti principali

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AvadaHeader/>
+    <AvadaMain/>
+    <AvadaFooter/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AvadaHeader from './components/AvadaHeader.vue'
+import AvadaMain from './components/AvadaMain.vue'
+import AvadaFooter from './components/AvadaFooter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AvadaHeader, AvadaMain, AvadaFooter
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/AvadaHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Header</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AvadaHeader',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
